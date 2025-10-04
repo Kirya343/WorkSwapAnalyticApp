446baf8eb8d17140374b2bf66ed9e8b5321b3450
Kirya <kkodolov40@gmail.com>
2025-10-04 10:46:23 +0300

    Настройка сохранения снапшота юзеров

--- src/main/java/org/workswap/stat/consumers/UsersStatConsumer.java
+++ src/main/java/org/workswap/stat/consumers/UsersStatConsumer.java
@@ -2,20 +2,20 @@ package org.workswap.stat.consumers;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.workswap.common.dto.stat.OnlineStatSnapshotDTO;
+import org.workswap.common.dto.stat.UsersStatSnapshotDTO;
 import org.workswap.core.services.command.StatisticCommandService;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class OnlineStatConsumer {
+public class UsersStatConsumer {
     
     private final StatisticCommandService statisticCommandService;
 
-    @RabbitListener(queues = "onlineStatQueue")
-    public void onlineStatQueue(OnlineStatSnapshotDTO dto) {
+    @RabbitListener(queues = "usersStatQueue")
+    public void usersStatQueue(UsersStatSnapshotDTO dto) {
         
-        statisticCommandService.saveOnlineStatSnapshot(dto);
+        statisticCommandService.saveUsersStatSnapshot(dto);
     }
 }
